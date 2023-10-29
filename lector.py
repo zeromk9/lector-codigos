dc2f419dd7f35a015d31fa4e14d2e2f9d6d72501
zeromk9 <zeromarthk@hotmail.com>
2023-10-29 13:54:13 -0600

    CopiarCodigosCarpetas

---
+++ lector.py
@@ -0,0 +1,43 @@
+import os
+import tkinter as tk
+from tkinter import filedialog
+
+# Abre un cuadro de diálogo para que el usuario seleccione un directorio
+root = tk.Tk()
+root.withdraw()  # Oculta la ventana principal de tkinter
+ruta_directorio = filedialog.askdirectory(title="Selecciona un directorio")
+
+if not ruta_directorio:
+    print("No se seleccionó ningún directorio. Saliendo del programa.")
+    exit()
+
+# Lista todos los archivos en el directorio y subdirectorios
+def listar_archivos(ruta):
+    lista_archivos = []
+    for foldername, subfolders, filenames in os.walk(ruta):
+        for filename in filenames:
+            lista_archivos.append(os.path.join(foldername, filename))
+    return lista_archivos
+
+# Lee el contenido de un archivo
+def leer_contenido(archivo):
+    with open(archivo, 'r', encoding='utf-8') as file:
+        return file.read()
+
+# Obtiene la lista de archivos y sus contenidos
+archivos = listar_archivos(ruta_directorio)
+datos_archivos = []
+
+for archivo in archivos:
+    nombre_archivo = os.path.basename(archivo)
+    contenido_archivo = leer_contenido(archivo)
+    datos_archivos.append(f'Nombre del archivo: {nombre_archivo}\nContenido:\n{contenido_archivo}\n{"="*50}')
+
+# Obtiene la ruta completa del archivo de texto en el directorio seleccionado
+ruta_archivo_txt = os.path.join(ruta_directorio, 'codigos.txt')
+
+# Escribe los datos en el archivo de texto (modo de adición)
+with open(ruta_archivo_txt, 'a', encoding='utf-8') as file:
+    file.write('\n'.join(datos_archivos))
+
+print(f'Archivos y contenidos agregados a {ruta_archivo_txt}')
